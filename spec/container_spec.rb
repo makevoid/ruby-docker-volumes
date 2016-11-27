fcd6c0447451d0db9ce6be0867a7f20accdedbf3
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-11-27 14:31:52 +0000

    add notes

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
+# TODO: implement id tracing and url assignation based on docker network setup (config file / env var) - use configatron or an alternative for managing configurations ( https://github.com/markbates/configatron )
+
+def container_id
+  1
+end
+
+def container_url
+  "http://localhost:32839"
+end
+
+# ---
+
 describe "Container" do
   it "clears active containers" do
     puts `docker-compose down`
@@ -10,7 +22,7 @@ describe "Container" do
   end
 
   it "healthchecks the apps" do
-    puts HTTParty.get "http://localhost:32839"
+    puts HTTParty.get container_url(container_id)
     puts `docker exec trydockervolumes_roda_1 ls /app/db`
   end
 end
